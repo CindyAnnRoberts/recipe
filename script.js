8214315ebae0acdc3e48f33483ea537f7c32143a
CindyAnnRoberts <115963721+CindyAnnRoberts@users.noreply.github.com>
2023-02-25 18:52:22 -0500

    add javaScript function

---
+++ script.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//     var hamburgerToggle = document.getElementById("menuLinks");
+//     if (hamburgerToggle.style.display === "block") {
+//         hamburgerToggle.style.display = "none";
+//     } else {
+//         hamburgerToggle.style.display = "block";
+//     }
+//   }
+
+  function myFunction() {
+    var hamburgerToggle = document.getElementById("menuLinks");
+    if (hamburgerToggle.style.display === "block") {
+        hamburgerToggle.style.display = "none";
+    } else {
+        hamburgerToggle.style.display = "block";
+    } 
+  }
